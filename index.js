e2d1271f6e4137cb401f807e23bd79a19479f0d8
thirstu <ajayknpc185@gmail.com>
2024-04-19 11:41:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const exMethod=express();
+
+const port =4000;
+
+exMethod.get('/',(req,res)=>{
+    res.send('hello world');
+});
+exMethod.get('/login',(req,res)=>{
+    res.send('hello login');
+});
+exMethod.get('/lone',(req,res)=>{
+    res.send('<h2>hello lone</h2>');
+});
+exMethod.listen(process.env.PORT,(req,res)=>{
+    console.log(`listening say, what! ${port}${process.env.PORT}`);
+})
